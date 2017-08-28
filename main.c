26a43a9b561922ca58df1e97ef391856f17bc6ae
heruwakame <s15te041@mail.saitama-u.ac.jp>
2017-08-28 13:39:45 +0900

    No use

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
 #include <stdint.h>
-
+#define GPIO_BASEADDR 0x20200000
+#define UART_BASEADDR 0x20201000
 static inline uint32_t setGPIO(uint32_t register_num, uint32_t mode, uint32_t index);
 
-/*
-static inline uint32_t setGPIO(uint32_t register_num, uint32_t mode, uint32_t index){
-	*((uint32_t*)(0x20200000 + register_num*0x4)) &= ~(uint32_t)(0x7  << (index * 3));
-	*((uint32_t*)(0x20200000 + register_num*0x4)) |=  (uint32_t)(mode << (index * 3));
-}
-*/
+static inline uint32_t setUART(uint32_t register_num, uint32_t mode, uint32_t index);
 
 int main(void)
 {
@@ -16,21 +12,30 @@ int main(void)
 	setGPIO(2, 0x3, 4);
 	setGPIO(2, 0x3, 5);
 	setGPIO(2, 0x3, 7);
+
 	setGPIO(1, 0x4, 4);
 	setGPIO(1, 0x4, 5);
+	
+	// UARTEN -> 0
+	*((uint32_t*)(UART_BASEADDR + 0x30)) &= ~(uint32_t)(0x1);
 
-	*((uint32_t*)(0x20201000 + 0x30)) &=  (uint32_t)0x0;
-	*((uint32_t*)(0x20201000 + 0x30)) |=  (uint32_t)0x0;
-	*((uint32_t*)(0x20201000 + 0x2c)) |=  (uint32_t)(0x0 << 4);
-	*((uint32_t*)(0x20201000 + 0x2c)) |=  (uint32_t)(0x0 << 3);
-	*((uint32_t*)(0x20201000 + 0x2c)) |=  (uint32_t)(0x0 << 1);
-	*((uint32_t*)(0x20201000 + 0x2c)) |=  (uint32_t)(0x3 << 5);	
-	*((uint32_t*)(0x20201000 + 0x30)) |=  (uint32_t)(0x1 << 8);
-	*((uint32_t*)(0x20201000 + 0x30)) |=  (uint32_t)(0x1 << 9);
-	*((uint32_t*)(0x20201000 + 0x24)) =  (uint32_t)0x1;
-	*((uint32_t*)(0x20201000 + 0x28)) =  (uint32_t)0x28;
-	*((uint32_t*)(0x20201000 + 0x30)) |=  (uint32_t)0x1;
+	// FIFO    -> None
+	*((uint32_t*)(UART_BASEADDR + 0x2c)) &= (uint32_t)(0x0 << 4);
+	// DataBit -> 8bit
+	*((uint32_t*)(UART_BASEADDR + 0x2c))  = (uint32_t)0x0;
+	*((uint32_t*)(UART_BASEADDR + 0x2c)) |= (uint32_t)(0x3 << 5);
+	// StopBit -> 1bit
+	//*((uint32_t*)(UART_BASEADDR + 0x2c)) &= (uint32_t)(0x0 << 3)
+	// Parity  -> None
+	//*((uint32_t*)(UART_BASEADDR + 0x2c)) &= (uint32_t)(0x0 << 1)
 
+	// Baudrate IBRD->1 FBRD->40
+	*((uint32_t*)(UART_BASEADDR + 0x24)) &= (uint32_t)0x1;
+	*((uint32_t*)(UART_BASEADDR + 0x28)) &= (uint32_t)0x28;
+
+	// UARTEN -> 1
+	*((uint32_t*)(UART_BASEADDR + 0x30)) |= (uint32_t)(0x1);
+	
 	uint32_t i = 0;
 
 	char str[11] = "HelloWorld";
@@ -45,6 +50,6 @@ int main(void)
 }
 
 static inline uint32_t setGPIO(uint32_t register_num, uint32_t mode, uint32_t index){
-	*((uint32_t*)(0x20200000 + register_num*0x4)) &= ~(uint32_t)(0x7  << (index * 3));
-	*((uint32_t*)(0x20200000 + register_num*0x4)) |=  (uint32_t)(mode << (index * 3));
+	*((uint32_t*)(GPIO_BASEADDR + register_num*0x4)) &= ~(uint32_t)(0x7  << (index * 3));
+	*((uint32_t*)(GPIO_BASEADDR + register_num*0x4)) |=  (uint32_t)(mode << (index * 3));
 }
